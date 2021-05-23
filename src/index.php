d98887756416adeff8cf88c32d9ca8cb5a55565c
Ahmed Abd Elghany <34424462+AhmedAbdElghany97@users.noreply.github.com>
2021-05-23 14:06:02 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,10 +1,12 @@
 <?php
 session_start();
 require_once "config.php";
-if(isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] == true){
-    header("location: login.php");
+
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    header("location: welcome.php");
     exit;
 }
+
 ?>
 
 
