7fdf418ffa2d9eda3e18d998e79c1078496dcfd6
Ahmed Abd Elghany <34424462+AhmedAbdElghany97@users.noreply.github.com>
2021-05-23 14:07:32 +0200

    Add files via upload

---
+++ src/thankyou.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Thanks For Registration</title>
+    <link rel="stylesheet" href="style.css">
+    <style>.error-msg{
+            font-size:16px; 
+            font-style: italic; 
+            display:block;
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="form">
+            Thanks For Your Regitration! <br>
+            Please <a href="welcome.php" style="text-decoration:none; color: red;">Login</a>.</b>
+        </div>
+    </div>
+    
+    
+    <script src='https://code.jquery.com/jquery-3.3.1.min.js'></script>
+    
+</body>
+</html>
